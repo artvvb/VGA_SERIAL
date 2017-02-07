33f4fc18683dd9a2f1e6fbe9ee206c3581820abd
Arthur Brown <arthur.brown@email.wsu.edu>
2017-02-07 12:20:30 -0800

    Implemented VGA Display of Line Length

--- src/hdl/top.v
+++ src/hdl/top.v
@@ -22,22 +22,20 @@
 
 module top_ascii(
     input clk,
-//    input btn,
-//    input [7:0] sw,
     output [15:0] led,
     output vs,
     output hs,
     output [3:0] vga_r,
     output [3:0] vga_g,
     output [3:0] vga_b,
-//    input rx,
     input tx
 );
     localparam  Idle                                 = 0,
                 MoveCursor                           = 1,
                 Write                                = 2,
                 UpdateLength                         = 3,
-                STATE_BITS                           = 2,
+                WriteLength                          = 4,
+                STATE_BITS                           = 3,
                 MAXX                                 = 79,
                 MAXY                                 = 59,
                 SHIFT_DEPTH                          = 3;
@@ -49,29 +47,34 @@ module top_ascii(
     reg  [23:0] rx_shift                             = 0;
     wire        rx_flag;
     wire  [7:0] rx_data;
-    wire [12:0] ascii_address;
-    
+    wire [12:0] cursor;
+    reg  [12:0] ascii_address;
     reg   [6:0] line_length [5:0];
     reg   [6:0] tlen;
+    reg   [7:0] ascii_data;
+    
     integer i;
     initial for (i=0; i<2**6; i=i+1) line_length[i] = 0;
     
-    assign ascii_address = cy * (MAXX+1) + cx;
+    assign cursor = cy * (MAXX+1) + cx;
     assign led = line_length[cy];
      
     always@(posedge clk)
         case(state)
         Idle: begin
+            ascii_wr_en <= 0;
             if (rx_flag == 1) begin // ensured to be one clock cycle long pulse
                 state <= Write;
-                state_shifter <= {Idle[1:0], MoveCursor[1:0], UpdateLength[1:0]};
+                state_shifter <= {Idle[STATE_BITS-1:0], MoveCursor[STATE_BITS-1:0], WriteLength[STATE_BITS-1:0], UpdateLength[STATE_BITS-1:0]};
                 rx_shift <= {rx_shift[15:0], rx_data}; // enter received data into the shift register
             end
         end
         Write: begin
-            state         <= state_shifter[1:0];
-            state_shifter <= (state_shifter >> 2);
-            tlen = line_length[cy];
+            state         <= state_shifter[STATE_BITS-1:0];
+            state_shifter <= (state_shifter >> STATE_BITS);
+            tlen          <= line_length[cy];
+            ascii_address <= cursor;
+            ascii_data    <= rx_shift[7:0];
             if (rx_shift[7:0] < 32)            ascii_wr_en <= 0; // character is not typable
             else if (rx_shift[23:16] == 8'h1b) ascii_wr_en <= 0; // recent escape code
             else if (rx_shift[15:8]  == 8'h1b) ascii_wr_en <= 0; // recent escape code
@@ -79,8 +82,8 @@ module top_ascii(
         end
         MoveCursor: begin
             ascii_wr_en   <= 0; // ensure write enable is one clock cycle long
-            state         <= state_shifter[1:0];
-            state_shifter <= (state_shifter >> 2);
+            state         <= state_shifter[STATE_BITS-1:0];
+            state_shifter <= (state_shifter >> STATE_BITS);
             if (rx_shift[23:16] == 8'h1b || rx_shift[15:8] == 8'h1b) begin
                 if      (rx_shift == {8'h1b, 8'h5b, 8'h41})     cy <= (cy == 0)    ? MAXY : (cy - 1); // up arrow
                 else if (rx_shift == {8'h1b, 8'h5b, 8'h42})     cy <= (cy == MAXY) ? 0    : (cy + 1); // down arrow
@@ -91,15 +94,34 @@ module top_ascii(
             else if     (rx_shift[7:0] == 10)   cy <= (cy == MAXY) ? 0 : (cy + 1); // LF
         end
         UpdateLength: begin
-            ascii_wr_en   <= 0; // ensure write enable is one clock cycle long
-            state         <= state_shifter[1:0];
-            state_shifter <= (state_shifter >> 2);
-            if (ascii_wr_en == 0) line_length[cy] <= tlen;
-            else                  line_length[cy] <= tlen + 1;
+            state         <= state_shifter[STATE_BITS-1:0];
+            state_shifter <= (state_shifter >> STATE_BITS);
+            if (ascii_wr_en == 1 && cx >= tlen) begin
+                ascii_wr_en     <= 1;
+                line_length[cy] <= tlen + 1;
+                tlen            <= (tlen + 1) / 10;
+                ascii_data      <= ((tlen + 1) % 10) + 8'h30;
+                ascii_address   <= cy * (MAXX + 1) + MAXX;
+            end else begin
+                ascii_wr_en     <= 0;
+                tlen            <= 0;
+            end
+        end
+        WriteLength: begin
+            if (tlen == 0) begin
+                state           <= state_shifter[STATE_BITS-1:0];
+                state_shifter   <= (state_shifter >> STATE_BITS);
+                ascii_wr_en     <= 0;
+            end else begin
+                tlen            <= (tlen / 10);
+                ascii_data      <= (tlen % 10) + 8'h30;
+                ascii_wr_en     <= 1;
+                ascii_address   <= ascii_address - 1;
+            end
         end
         endcase
     
-    vga_2buf mVGA ( clk, ascii_address, rx_shift[7:0], ascii_wr_en, vs, hs, vga_r, vga_g, vga_b, ascii_address, 0 );
+    vga_2buf mVGA ( clk, ascii_address, ascii_data, ascii_wr_en, vs, hs, vga_r, vga_g, vga_b, cursor, 0 );
     uart_rx  mRX  ( clk, rx_flag, rx_data, tx );
     
 endmodule
